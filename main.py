dd91b27ac868c670ec27048f1b031bc23abdebb6
pasyanos <pasyanos.c@gmail.com>
2024-09-13 16:33:23 -0400

    reading from file

--- main.py
+++ main.py
@@ -6,4 +6,21 @@ if __name__ == '__main__':
     # take in arguments
     parser = argparse.ArgumentParser()
 
-    print('Hello, World!')
+    # require an input file to read from
+    parser.add_argument('input', help='Input file to read from', type=str)
+
+    # todo: optional arguments here
+
+    args = parser.parse_args()
+    fileName = args.input
+
+    # open and read points from file
+    with open(fileName, 'r') as file:
+        points = file.readlines()
+
+    # remove newline characters
+    points = [point.strip() for point in points]
+
+    print(points)
+
+
